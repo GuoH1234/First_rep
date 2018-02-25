8fd84ca5ca71dd51846d7092246e2237794d139b
GuoH1234 <641284712@qq.com>
2018-02-25 10:26:35 +0800

    Revert "修改"
    This reverts commit d526273f3d6cb8b831b6239107a4e1288caf44e6.

--- ShootController.cs
+++ ShootController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
--------------------------------------
 public class ShootController : MonoBehaviour {
 	public WeaponBase wea_InHand,wea_InPack;
 	public Transform GunPoint;	
