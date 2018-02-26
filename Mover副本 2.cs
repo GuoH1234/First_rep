0ec43b860937bc0c65ca06c4c026860fe7c9c833
GuoH1234 <641284712@qq.com>
2018-02-26 09:21:30 +0800

    2
    我问问

---
+++ Mover副本 2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+//修改后的文件
+public class Mover : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
